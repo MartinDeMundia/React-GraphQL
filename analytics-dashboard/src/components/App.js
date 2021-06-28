638a67b863bbd9d5db54ab395e6e4b921c7e4df5
Martin M. Mundia <martin@nts.nl>
2021-06-28 16:03:18 +0300

    Final copy of my work

--- analytics-dashboard/src/components/App.js
+++ analytics-dashboard/src/components/App.js
@@ -15,7 +15,7 @@ class App extends Component {
         <div class="navbar-header">
             <ul class="nav navbar-nav flex-row">
                 <li class="nav-item mr-auto"><a href="/" class="navbar-brand" >                       
-                        <h2 class="brand-text mb-0">Core Movies</h2>
+                        <h2 class="brand-text mb-0">ACCESS AFYA</h2>
                     </a></li>
                 <li class="nav-item nav-toggle"><a href="/" class="nav-link modern-nav-toggle pr-0" data-toggle="collapse"><i class="feather icon-x d-block d-xl-none font-medium-4 primary toggle-icon"></i><i class="toggle-icon feather icon-disc font-medium-4 d-none d-xl-block collapse-toggle-icon primary" data-ticon="icon-disc"></i></a></li>
             </ul>
